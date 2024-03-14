10b90c8c1e8816492dce755e401725de4a252417
Luc Van Christopher ANDRIANAVALONIRINA <lucvanchristopher.andrianavalonirina2023@campus-eni.fr>
2024-03-14 15:37:39 +0100

    Ajoutez des fichiers projet.

---
+++ MyContact/Pages/Clients/Index.cshtml.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace MyContact.Pages.Clients
+{
+    public class IndexModel : PageModel
+    {
+        public List<ClientInfo> Listclients = new List<ClientInfo>();
+
+        public void OnGet()
+        {
+            try
+            {
+                string connectionString = "Data Source=MSI;Initial Catalog=MyContact;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sql = "SELECT * FROM Clients";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                ClientInfo clientInfo = new ClientInfo();
+                                clientInfo.id = reader.GetInt32(0);
+                                clientInfo.name = reader.GetString(1);
+                                clientInfo.email = reader.GetString(2);
+                                clientInfo.phone = reader.GetString(3);
+                                clientInfo.address = reader.GetString(4);
+                                clientInfo.created_at = reader.GetDateTime(5).ToString();
+                                Listclients.Add(clientInfo);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(" EXCEPTION" + ex.Message);
+            }
+        }
+    }
+
+    public class ClientInfo
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public string created_at { get; set; }
+    }
+}
+
+
+
+//Server = localhost; Database = MyContact; User Id = sa; Password = 123456
---
+++ MyContact/Pages/Clients/create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyContact.Pages.Clients
+{
+    public class createModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
